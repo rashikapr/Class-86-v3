4978242881c9c615f1ab0e9d717ad7a5aae5c5e9
rashikapr <74289013+rashikapr@users.noreply.github.com>
2023-02-01 22:47:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyADRJs2__X6avZtyn8nsK0EOLmv_L6fKYA",
+  authDomain: "wireless-buzzer-ef101.firebaseapp.com",
+  databaseURL: "https://wireless-buzzer-ef101-default-rtdb.firebaseio.com",
+  projectId: "wireless-buzzer-ef101",
+  storageBucket: "wireless-buzzer-ef101.appspot.com",
+  messagingSenderId: "61643369732",
+  appId: "1:61643369732:web:cc6085eaf1b1c4cdaeb451",
+  measurementId: "G-HDYN3G3E2W"
+};
+
